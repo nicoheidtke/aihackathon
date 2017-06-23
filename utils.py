4fb0b6ee012c1ad24940753f4e47755aecf293cd
ssukhanov <ssukhanov@agtinternational.com>
2017-06-23 23:28:07 +0200

    v0 beta: similarity between tweet under consideration and storage

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial.distance import cosine
 from spacy.en import English
 parser = English()
 
@@ -11,6 +14,34 @@ def transform_tweet(tweet):
     return output
 
 
+def put_gt_tweet_in_storage(tweet, df):
+    for entity, entity_type, vector_array in transform_tweet(tweet):
+        df = pd.concat([df, pd.DataFrame([{'Entity': entity, 'Entity type': entity_type, 'Vector array':vector_array}])], axis=0)
+    return df
+
+
+def compare_tweet_with_storage(tweet, storage):
+    transformed_tweet = transform_tweet(tweet)
+    scores = {}
+    for i, (entity, entity_type, vector_array) in enumerate(transformed_tweet):
+        temp_score = 0
+        for j, (_, item) in enumerate(storage[storage['Entity'] == entity].iterrows()):
+            # similarity
+            temp_score = np.max([1 - cosine(vector_array, item['Vector array']), temp_score])
+            print(1 - cosine(vector_array, item['Vector array']), entity, tweet, str(j))
+        scores.update({entity: temp_score})
+    return scores
+
 if __name__ == '__main__':
-    tweet = u"Donald Trump was killed yesterday!"
-    out = transform_tweet(tweet)
+    tweet_to_check = u"Donald Trump was murdered yesterday!"
+
+    tweets = [u"Donald Trump was killed yesterday!",
+              u"Donald Trump is gay",
+              u"Donald Trump killed his wife!",
+              u"Donald Trump launched a rocket and started a world war!"]
+    df = pd.DataFrame(columns=['Entity', 'Entity type', 'Vector array'])
+    for tweet in tweets:
+        df = put_gt_tweet_in_storage(tweet, df)
+    print(df.head())
+    scores = compare_tweet_with_storage(tweet_to_check, df)
+    print(scores)
